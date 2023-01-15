e29a8fe1bb71335604e676b8556efe95fcf41578
Serhii Karanin <serjkaranin@gmail.com>
2023-01-15 19:22:43 +0200

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+      let stringNumber = `${n}`
+    if (n <= 0) {
+        return stringNumber.split("").reverse().join("").slice(0,-1);
+    }
+    else return stringNumber.split("").reverse().join("");
 }
